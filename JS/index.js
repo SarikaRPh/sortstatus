5ee525c24d92cecfab0b63044f0d8cc9f158e02c
Sarika Phule <sarika.raskar@gmail.com>
2021-10-12 16:47:07 +1100

    sortstatus

---
+++ JS/index.js
@@ -0,0 +1,302 @@
+//get the id from index.html
+const taskManager = new TaskManager(0);
+
+taskManager.load();
+taskManager.render();
+
+
+const  myName = document.querySelector('#myName');
+const  floatingTextarea2 = document.querySelector('#floatingTextarea2');
+const  assign = document.querySelector('#assign');
+const  status1 = document.querySelector('#status');
+const  date = document.querySelector('#date');
+let validationFail = 0;
+const submit = document.querySelector('#submit');
+const doneButton = document.querySelector(".done-button");
+const sortTask = document.querySelector("#sort-task");
+
+//edit Modal variable
+const edit_myName = document.querySelector('#edit_myName');
+const edit_floatingTextarea2 = document.querySelector('#edit_floatingTextarea2');
+const edit_assign = document.querySelector('#edit_assign');
+const edit_status = document.querySelector('#edit_status');
+const edit_date = document.querySelector('#edit_date');
+const Update = document.querySelector('#Update');
+ 
+ var today = new Date().toISOString().split('T')[0];
+ //console.log("today" + today);
+ date.setAttribute("min",today);
+ 
+const validFormFieldInput = (event) => {
+  
+event.preventDefault();
+  const clearFormFields = () => {
+    myName.value = "";
+    floatingTextarea2.value = "";
+    assign.value = "";
+    status1.value = "";
+    date.value = "";
+    myName.classList.remove("is-valid");
+    floatingTextarea2.classList.remove("is-valid");
+    assign.classList.remove("is-valid");
+    status1.classList.remove("is-valid");
+    date.classList.remove("is-valid");
+  };
+
+
+ 
+
+
+    //checking task name//
+    if (myName.value.length > 5) {
+        myName.classList.add("is-valid");
+        myName.classList.remove("is-invalid");
+      } else {
+        myName.classList.add("is-invalid");
+        myName.classList.remove("is-valid");
+         validationFail++;
+      }
+
+    //end task name
+
+      
+
+    //check description//
+    if ( floatingTextarea2.value.length > 5) {
+        floatingTextarea2.classList.add("is-valid");
+        floatingTextarea2.classList.remove("is-invalid");
+      } else {
+        floatingTextarea2.classList.add("is-invalid");
+        floatingTextarea2.classList.remove("is-valid");
+         validationFail++;
+      }
+    //end discription//
+
+
+
+    //check assign//
+    if (assign.value.length > 5) {
+        assign.classList.add("is-valid");
+        assign.classList.remove("is-invalid");
+      } else {
+        assign.classList.add("is-invalid");
+        assign.classList.remove("is-valid");
+         validationFail++;
+      }
+    
+      //end assign//
+    
+
+     //check status//
+     if (status1.value !== 'Choose') {
+        status1.classList.add("is-valid");
+        status1.classList.remove("is-invalid");
+      } else {
+        status1.classList.add("is-invalid");
+        status1.classList.remove("is-valid");
+         validationFail++;
+      }
+     //end status//
+
+     
+      // check due date
+
+     if (date.value){
+        date.classList.add("is-valid");
+        date.classList.remove("is-invalid");
+      } else {
+        date.classList.add("is-invalid");
+        date.classList.remove("is-valid");
+         validationFail++;
+      }
+     //end due date
+
+     
+     if (validationFail > 0) {
+      validationFail = 0;
+      //clearFormFields();
+      return;
+    } else {
+      // Push the valid input into our tasks array
+      console.log("Task Name :" + myName.value);
+      console.log("Task Description :" + floatingTextarea2.value);
+      console.log("Task Assigned To :" + assign.value);
+      console.log("Task Due Date :" + status1.value);
+      console.log("Task Status:" + date.value);
+      taskManager.addTask(
+        myName.value,
+        floatingTextarea2.value,
+        assign.value,
+        status1.value,
+        date.value
+      );
+      taskManager.save();
+      taskManager.render();
+      clearFormFields();
+      $("#exampleModal").modal("hide");
+    }
+      
+     
+  }
+
+  //editValidFormFieldInput function
+  const editValidFormFieldInput = event => {
+    event.preventDefault();
+     //checking task name//
+     if (edit_myName.value.length > 5) {
+      edit_myName.classList.add("is-valid");
+      edit_myName.classList.remove("is-invalid");
+    } else {
+      edit_myName.classList.add("is-invalid");
+      edit_myName.classList.remove("is-valid");
+       validationFail++;
+    }
+
+  //end task name
+  //check description//
+  if ( edit_floatingTextarea2.value.length > 5) {
+    edit_floatingTextarea2.classList.add("is-valid");
+    edit_floatingTextarea2.classList.remove("is-invalid");
+  } else {
+    edit_floatingTextarea2.classList.add("is-invalid");
+    edit_floatingTextarea2.classList.remove("is-valid");
+     validationFail++;
+  }
+//end discription//
+
+ //check assign//
+ if (edit_assign.value.length > 5) {
+  edit_assign.classList.add("is-valid");
+  edit_assign.classList.remove("is-invalid");
+} else {
+  edit_assign.classList.add("is-invalid");
+  edit_assign.classList.remove("is-valid");
+   validationFail++;
+}
+
+//end assign//
+
+//check status//
+if (edit_status.value !== 'Choose') {
+  edit_status.classList.add("is-valid");
+  edit_status.classList.remove("is-invalid");
+} else {
+  edit_status.classList.add("is-invalid");
+  edit_status.classList.remove("is-valid");
+   validationFail++;
+}
+//end status//
+
+
+      // check due date
+
+      if (edit_date.value){
+        edit_date.classList.add("is-valid");
+        edit_date.classList.remove("is-invalid");
+      } else {
+        edit_date.classList.add("is-invalid");
+        edit_date.classList.remove("is-valid");
+         validationFail++;
+      }
+     //end due date
+
+ 
+     if (validationFail > 0) {
+      validationFail = 0;
+      //clearFormFields();
+      return;
+    } else {
+      // Push the valid input into our tasks array
+      taskManager.updateTask(
+        edit_myName.value,
+        edit_floatingTextarea2.value,
+        edit_assign.value,
+        edit_status.value,
+        edit_date.value,
+        edit_taskId.value
+  
+      );
+      taskManager.save();
+      taskManager.render();
+      // clearFormFields();
+      $("#edit-exampleModal").modal("hide");
+    }
+      
+     
+  }
+
+//validate and add task
+ submit.addEventListener('click',validFormFieldInput);
+
+
+ const taskList = document.querySelector(".task-list");
+
+ taskList.addEventListener('click',event=>{
+   if(event.target.classList.contains("done-button")){
+    const parentTask =
+    event.target.parentElement.parentElement.parentElement.parentElement;
+    console.log(parentTask);
+
+    const taskId = Number(parentTask.dataset.taskId);
+    
+    console.log(parentTask.dataset);
+
+    let task = taskManager.getTaskById(taskId);
+    
+    
+    task.status1 = 'Done';
+    
+    taskManager.render();
+    
+   }
+
+
+   //edit task 
+  //  if(event.target.classList.contains("edit-button")){
+  //   const parentTask =
+  //   event.target.parentElement.parentElement.parentElement.parentElement;
+  //   const taskId = Number(parentTask.dataset.taskId);
+  //   let task = taskManager.getTaskById(taskId);
+  //   let taskEdit = taskManager.editTask(taskId);
+  //       //Update button
+  //  Update.addEventListener('click', editValidFormFieldInput);
+  //  }
+
+
+   taskManager.save();
+   taskManager.render();
+
+   //delete task 
+    if(event.target.classList.contains("delete-button")){
+      const parentTask =
+    event.target.parentElement.parentElement.parentElement.parentElement;
+    const taskId = Number(parentTask.dataset.taskId);
+    //let task = taskManager.getTaskById(taskId);
+    taskManager.delete(taskId);
+    }
+    // taskManager.save();
+    // taskManager.render();
+
+    //edit task
+
+    if(event.target.classList.contains("edit-button")){
+      const parentTask =
+      event.target.parentElement.parentElement.parentElement.parentElement;
+      const taskId = Number(parentTask.dataset.taskId);
+      taskManager.editTask(taskId);
+      let task = taskManager.getTaskById(taskId);
+   //Update button
+   Update.addEventListener('click', editValidFormFieldInput);
+     
+    }
+    taskManager.save();
+    taskManager.render();
+
+
+   
+ });
+
+ 
+ 
+
+
---
+++ JS/taskManager.js
@@ -0,0 +1,246 @@
+//creat createtaskhtml 
+const createTaskHtml = (myName, floatingTextarea2, assign, status1, date, id ) => {
+    // let todayDate = new Date();
+    // let formatDate = (todayDate.getDate() +  "/" +(todayDate.getMonth()+1 )+ "/" + todayDate.getFullYear());
+    const html = ` 
+    
+    <div class="mt-5 me-2 card ${status1 === "Done"? "border-success text-success" : status1 === "In Progress" ? "border-warning text-warning": status1 === "Review" ? "border-danger text-danger": "border-info text-info"}" style="width: 25rem;" data-task-id="${id}" >
+      <div class=" h-100">
+
+      <div class="card-body">
+        <h5 class="card-text fw-bold d-inline">Task Name : </h5>
+        <p class="d-inline">${myName}</p>
+        <p class="card-text mt-4"> <strong>Description :</strong> </p>
+        <p class="card-text">${floatingTextarea2}</p>
+        <p class="card-text"><strong>Assigned To :</strong> ${assign} </p>
+        <p class="card-text"><strong>Status :</strong>  <span> ${status1}</span></p>
+        <p class="card-text"><strong>Date :</strong> ${date}</p>       
+        
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+        <button  class="btn btn-primary done-button ${status1 === "Done" ? "invisible" : "visible"}"
+         type="button" id="done">Done</button>
+          <button class="btn btn-primary me-md-2 edit-button" type="button">Edit</button>
+          <button class="btn btn-primary delete-button" type="button">Delete</button>
+          
+        </div>
+        </div>
+        </div>
+        </div>`;
+    return html;
+
+}
+
+//create taskManager
+class TaskManager {
+    constructor(currentId = 0){
+        this._tasks = [];
+        this.currentId = currentId;
+        
+    }
+
+    //method to get task ID
+    getTaskById(taskId){
+        let foundTask;
+        for(let i=0; i<this.tasks.length; i++){
+            let task = this.tasks[i];
+            if(task.id === taskId){
+                foundTask = task;
+            }
+         }
+         return foundTask;
+    }
+
+
+    // function to delete task
+
+    delete(taskId){
+        const newTasks = [];
+       this._tasks.forEach(currentTask =>{
+        if(currentTask.id !== taskId ){
+            newTasks.push(currentTask);
+         }
+     this._tasks = newTasks;
+       });
+     }
+    
+    //function to add task
+    addTask(myName, floatingTextarea2, assign, status1, date){
+             
+        const newTask = {
+            id: this.currentId++,
+            myName: myName,
+            floatingTextarea2: floatingTextarea2,
+            assign: assign,
+            status1: status1,
+            date: date
+        };
+        this._tasks.push(newTask);
+
+
+    }
+
+    //function to Update task
+    updateTask(edit_myName, edit_floatingTextarea2, edit_assign, edit_status, edit_date, edit_taskId){
+            console.log('inside update') 
+       for(let i=0; i<this._tasks.length; i++){
+            console.log('outside if'+ this._tasks[i].id + '==' + edit_taskId )
+            if(this._tasks[i].id == edit_taskId ) {
+                console.log('inside if') 
+                // console.log('inside if'+ currentTask.id + '==' + edit_taskId )
+                    this._tasks[i].myName = edit_myName,
+                    this._tasks[i].floatingTextarea2 = edit_floatingTextarea2,
+                    this._tasks[i].assign =edit_assign,
+                    this._tasks[i].status1 = edit_status,
+                    this._tasks[i].date = edit_date 
+           
+            }
+
+    }
+}
+
+
+    get tasks(){
+        return this._tasks;
+    }
+
+    set tasks(_tasks){
+        return this._tasks;
+    }
+
+
+    //add render method
+
+    render(){
+       
+    let tasksHtmlList =[];
+    const doneButton = document.querySelector(".done-button");
+    for(let i=0; i<this.tasks.length; i++){
+        let currentTask = this.tasks[i];
+        let date = new Date(currentTask.date);
+        let formatDate = (date.getDate() +  "/" +(date.getMonth()+1 )+ "/" + date.getFullYear()); 
+        let taskHtml = createTaskHtml(currentTask.myName, currentTask.floatingTextarea2, currentTask.assign, currentTask.status1, formatDate, currentTask.id);
+        tasksHtmlList.push(taskHtml);
+        let newTaskhtml = tasksHtmlList.join('\n');
+    console.log(currentTask.status1);
+    console.log(typeof currentTask.status1);
+    if(currentTask.status1 === "To Do"){
+        console.log("inside to do");
+    const html = document.querySelector('#to_do');
+    html.innerHTML = newTaskhtml;
+    } else if(currentTask.status1 === "Pending"){
+        const  html= document.querySelector('#pending');
+        html.innerHTML = newTaskhtml;
+    } else if(currentTask.status1 === "Done"){
+        const  html= document.querySelector('#done');
+        html.innerHTML = newTaskhtml;
+    } else if (currentTask.status1 === "In Progress"){
+        const  html= document.querySelector('#progress');
+        html.innerHTML = newTaskhtml;
+    }
+    
+    }
+    
+    }
+
+
+    // Save Method start
+ save(){
+    console.log('inside save')
+    const tasksJson = JSON.stringify(this.tasks);
+
+    localStorage.setItem("tasks",tasksJson);
+    console.log(localStorage);
+    const currentId = String(this.currentId);
+    localStorage.setItem('currentId',currentId);
+ }
+ // load Method start
+ load(){
+ 
+    if(localStorage.getItem('tasks')){
+       // console.log('inside if')
+        let tasksJson = localStorage.getItem('tasks');
+        console.log(tasksJson);
+       this._tasks = JSON.parse(tasksJson);
+       console.log(this._tasks)
+
+    }
+    if(localStorage.getItem('currentId')){
+        let currentId = localStorage.getItem('currentId');
+       this.currentId = Number(currentId);
+    }
+
+ }
+ //delete button
+ delete(taskId){
+    const newTasks = [];
+   this._tasks.forEach(currentTask =>{
+    if(currentTask.id !== taskId ){
+        newTasks.push(currentTask);
+     }
+ this._tasks = newTasks;
+   });
+ }
+ //Edit button
+
+ editTask(taskId){
+    console.log(`edit task ID`+taskId);
+    $('#edit-exampleModal').modal('show');
+    const edit_myName = document.querySelector("#edit_myName")
+    const edit_taskId = document.querySelector("#edit_taskId")
+    const edit_floatingTextarea2 = document.querySelector("#edit_floatingTextarea2");
+    const edit_assign = document.querySelector("#edit_assign");
+    const edit_status = document.querySelector("#edit_status");
+    const edit_date = document.querySelector("#edit_date");
+    for (let i = 0; i< this._tasks.length; i++){
+        const task = this._tasks[i];
+        if(task.id === taskId){
+            edit_taskId.value = taskId;
+            edit_myName.value = task.myName;
+            edit_floatingTextarea2.value =task.floatingTextarea2
+            edit_assign.value = task.assign;
+            edit_status.value = task.status1;
+            edit_date.value = task.date;
+            }
+        }
+        // return task;            
+}
+
+
+ //edit method start
+
+ editTask(taskId){
+    console.log(`edit task ID`+taskId);
+    $('#edit-exampleModal').modal('show');
+    const edit_myName = document.querySelector("#edit_myName")
+    const edit_floatingTextarea2 = document.querySelector("#edit_floatingTextarea2");
+    const edit_assign = document.querySelector("#edit_assign");
+    const edit_status = document.querySelector("#edit_status");
+    const edit_date = document.querySelector("#edit_date");
+
+
+    for (let i = 0; i< this._tasks.length; i++){
+        const task = this._tasks[i];
+
+        if(task.id === taskId){
+            edit_taskId.value = taskId;
+            edit_myName.value = task.myName;
+            edit_floatingTextarea2.value =task.floatingTextarea2
+            edit_assign.value = task.assign;
+            edit_status.value = task.status1;
+            edit_date.value = task.date;
+            }
+        }
+       // return task;            
+}
+
+ 
+
+ 
+
+}
+
+
+
+
+
+
